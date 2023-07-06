6c2cd2eaa67935c774d91c1894c9ba569e02212d
Aditya Khade <khadeaditya1@gmail.com>
2023-07-06 13:35:17 +0530

    change Title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
             <div>
                 <nav className="navbar  navbar-expand-lg  bg-dark" data-bs-theme="dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">Informedia</a>
+                        <a className="navbar-brand" href="/">Aditya Khade</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
